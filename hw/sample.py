896deb573b774ed96827435aff023a7bb620d115
kanan132 <63307356+kanan132@users.noreply.github.com>
2020-06-10 16:12:15 +0400

    Add files via upload

--- hw/sample.py
+++ hw/sample.py
@@ -1,7 +1,10 @@
 for i in range(1,101):
     
-    if i%15==0:
-        print(f"{i}-FizzBuzz")
+    if i%3==0:
+        if i%5==0:
+            print(f"{i}-FizzBuzz")
+        else:
+            print(f"{i}-fizz")
     elif i%3==0:
         print(f"{i}-fizz")
     elif i%5==0:
